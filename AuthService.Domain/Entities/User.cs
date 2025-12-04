cba9e0ff889458ab189773504f261e0c26bd93d6
A4nold <chinonsoekechi@gmail.com>
2025-12-04 20:10:50 +0000

    setting up authservice

---
+++ AuthService.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Email { get; set; } = null!;
+        public string Alias { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public List<UserRole> UserRoles { get; set; } = new();
+
+    }
+}
